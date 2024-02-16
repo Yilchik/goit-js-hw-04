8353ee1484ed42b39a190c576154a6accb533568
Yilchik <yulyayulia2@gmail.com>
2024-02-16 18:11:50 +0100

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,20 @@
-function slugify(title) {
-  let titleNew = title.toLowerCase();
-  let array = titleNew.split(' ');
-  let slug = array.join('-');
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  const productSize = Object.values(products);
+  let productSumm = 0;
+  for (const item of productSize) {
+    productSumm += item;
+  }
+  if (productSumm <= containerSize) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
